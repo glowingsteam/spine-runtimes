7821ffc5506503b66c55bee782e47a7c7d9c7115
yihui-hu <yihui.hu@tufts.edu>
2025-04-02 10:49:16 -0400

    Add custom `renderToImage` function for sticker exporting, and `disposeForSharedDrawable`
    - Documented in the code itself, but TLDR is the o.g. `renderToImage` function is not well suited for extracting frames for animations due to memory and perf issues
    - Also `disposeForSharedDrawable` is for disposing the drawable created via the shared `.atlas` and `.skel` file without discarding them as well (bc they're likely in use for other Spine models)

--- spine-ios/Sources/Spine/Extensions/SkeletonDrawableWrapper+CGImage.swift
+++ spine-ios/Sources/Spine/Extensions/SkeletonDrawableWrapper+CGImage.swift
@@ -51,4 +51,111 @@ public extension SkeletonDrawableWrapper {
         }
         return cgImage
     }
+    
+    // AN_FIX - Custom renderToImage for exporting animations
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // The original `renderToImage` function above is not well suited for exporting animations
+    // because it creates a new `SpineUIView` for *every* single frame.
+    //
+    // Instead, we should just set up a singular render context and SpineView to use throughout
+    // the exporting session.
+    //
+    // Please see `extractFramesFromAnimation` in `StickerExportManager` for more info on usage.
+    //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    /// This function is used to set up a singular `SpineUIView`
+    /// and `SpineController` to be used throughout the
+    /// sticker exporting session.
+    ///
+    func setupRenderContext(size: CGSize) throws -> (
+        SpineUIView,
+        SpineController
+    ) {
+        let controller = SpineController(disposeDrawableOnDeInit: false)
+        
+        let spineView = SpineUIView(
+            controller: controller,
+            backgroundColor: .clear
+        )
+                
+        spineView.frame = CGRect(origin: .zero, size: size)
+        spineView.isPaused = false
+        spineView.enableSetNeedsDisplay = false
+        spineView.framebufferOnly = false
+        
+        try spineView.load(drawable: self)
+        spineView.renderer?.waitUntilCompleted = true
+        
+        return (spineView, controller)
+    }
+
+    /// Renders a frame using a singular `SpineUIView`
+    /// that should be set up via `setupRenderContext`.
+    ///
+    func renderFrameWithContext(
+        spineView: SpineUIView
+    ) throws -> CGImage? {
+        spineView.delegate?.draw(in: spineView)
+        
+        guard let currentDrawable = spineView.currentDrawable else {
+            throw SpineError("Could not get drawable.")
+        }
+        
+        let texture = currentDrawable.texture
+        
+        let width = texture.width
+        let height = texture.height
+        let rowBytes = width * 4
+        
+        return try autoreleasepool {
+            let data = UnsafeMutableRawPointer.allocate(
+                byteCount: rowBytes * height,
+                alignment: MemoryLayout<UInt8>.alignment
+            )
+            
+            defer { data.deallocate() }
+            
+            let region = MTLRegionMake2D(0, 0, width, height)
+            texture.getBytes(data, bytesPerRow: rowBytes, from: region, mipmapLevel: 0)
+            
+            let bitmapInfo = CGBitmapInfo(
+                rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue
+            ).union(.byteOrder32Little)
+            
+            let colorSpace = CGColorSpaceCreateDeviceRGB()
+            
+            guard let context = CGContext(
+                data: data, width: width, height: height,
+                bitsPerComponent: 8, bytesPerRow: rowBytes,
+                space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+            else {
+                throw SpineError("Could not create context.")
+            }
+            
+            guard let cgImage = context.makeImage() else {
+                throw SpineError("Could not create image.")
+            }
+            
+            return cgImage
+        }
+    }
+    
+    /// Remember to clean up the `SpineUIView`
+    /// and `SpineController`.
+    ///
+    func cleanupRenderContext(
+        spineView: SpineUIView,
+        controller: SpineController
+    ) {
+        spineView.delegate = nil
+        spineView.renderer = nil
+        
+        controller.drawable?.disposeForSharedDrawable()
+        controller.drawable = nil
+    }
+    
+    // AND_FIX_END - custom renderToImage
 }
